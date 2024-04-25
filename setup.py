bb47bc1ea066ad117de36266e122c0874225d45c
Maxime Armstrong <maxime@retain.ai>
2024-04-25 17:57:19 -0400

    Add versions for langchain dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="with_openai",
     packages=find_packages(exclude=["with_openai_tests"]),
-    install_requires=["dagster", "dagster-openai", "langchain==0.1.11", "dagster-cloud", "filelock", "langchain-community", "langchain-openai", "faiss-cpu"],
+    install_requires=["dagster", "dagster-openai", "langchain==0.1.11", "dagster-cloud", "filelock", "langchain-community==0.0.34", "langchain-openai==0.1.3", "faiss-cpu==1.8.0"],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
