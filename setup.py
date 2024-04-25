b7ef104ac0ea5def972b407fcef71ff97346afd4
Maxime Armstrong <maxime@retain.ai>
2024-04-24 20:44:39 -0400

    Fix dependencies

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="with_openai",
     packages=find_packages(exclude=["with_openai_tests"]),
-    install_requires=["dagster", "dagster-openai", "langchain==0.1.11", "dagster-cloud", "filelock"],
+    install_requires=["dagster", "dagster-openai", "langchain==0.1.11", "dagster-cloud", "filelock", "git"],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
